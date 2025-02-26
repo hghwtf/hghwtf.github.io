6b8688c8b06d3d3bce694a19a513df0fdb297387
jd <34028017+hghwtf@users.noreply.github.com>
2025-02-26 20:28:16 +0000

    font changes and titles of index

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,24 +8,24 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "jdru.dev",
+    pageTitleSuffix: ":)",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jdru.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Roboto Mono",
+        body: "Eoboto Flex",
+        code: "Inconsolata",
       },
       colors: {
         lightMode: {
